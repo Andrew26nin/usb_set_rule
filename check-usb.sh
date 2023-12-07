4b46204ea3f16799c9988c388549cd569c094ae9
Андрей Тарасенко <tarasenko@algont.ru>
2023-12-07 23:40:21 +0300

    add: Добавлен скрипт для проверки мониторинга подкдлючаемых USB

---
+++ check-usb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+udevadm monitor --udev --subsystem-match=block | while read -r line; do
+	# Проверяем, что это событие подключения
+	if echo $line | grep -q 'UDEV.*add.*sd[b-z][0-9]*'; then
+		# Извлекаем имя устройства
+		device=$(echo $line | awk '{print $4}' | awk -F'/' '{print $NF}')
+		echo "$device"
+	fi
+done
